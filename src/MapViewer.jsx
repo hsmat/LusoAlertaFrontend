c79757afa5fc82c80cbef1ad275d169bdd295d42
fccba <154232906+fccba@users.noreply.github.com>
2026-04-15 17:49:50 +0100

    Mapa Funcional a falar com o servidor. se for eventualmente publicado, temos de usar o arcgisonline, em vez do basemap da google

--- src/MapViewer.jsx
+++ src/MapViewer.jsx
@@ -1,40 +1,62 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MapControl from "./MapControl";
 
 const bounds = [ //cordenadas da area do porto
-  [40.55547653836869, -9.00023981916341], 
-  [41.55184466961709, -7.68380938985860] 
+  [40.555476, -9.000239], // South-West corner
+  [41.551844, -7.683809]  // North-East corner
 ];
 
 
 export default function MapViewer() {
-    const [ndvi, setSatellite] = useState(false);
+    const [ndvi, setNdvi] = useState(false);
+    const [ndviUrl, setNdviUrl] = useState("");
+
+    useEffect(() => {
+        // Fetch the Earth Engine tile URL only when NDVI is toggled on
+        if (ndvi && !ndviUrl) {
+            fetch("http://localhost:3000/mapid")
+                .then((res) => res.json())
+                .then((data) => {
+                    setNdviUrl(data.url);
+                })
+                .catch(console.error);
+        }
+    }, [ndvi, ndviUrl]);
+
+    //google satellite as an alternative base map
+    const baseMapUrl = "http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}";
+    const baseMapAttribution = 'Tiles &copy; Google';
+
+    // Use arcgis as a reliable base map.
+    // const baseMapUrl = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}";
+    // const baseMapAttribution = 'Tiles &copy; Esri';
+    
+
+    // Switch between the base map and the fetched NDVI layer URL
+    const tileUrl = ndvi ? ndviUrl : baseMapUrl;
 
     return (
         <MapContainer
-        center={[41.1, -8.7]}
-        zoom={9} 
+        center={[40.1598, -7.9842]}
+        zoom={16} 
         minZoom={9}
-        maxZoom={12}          
+        maxZoom={18}          
         style={{ height: "500px", width: "100%" }}
         maxBounds={bounds}
         maxBoundsViscosity={1.0}
         >
-        <TileLayer
-            url={
-            ndvi
-                ? "http://localhost:3000/tiles/ndvi/{z}/{x}/{y}"
-                : "http://localhost:3000/tiles/color/{z}/{x}/{y}"
-            }
-            
-            
-            tileSize={256}
-            tms={true} 
-            attribution="&copy; My Local Tiles"
-        />
-        <MapControl onToggle={() => setSatellite((s) => !s)} />
+        {/* Only render if we have a valid URL to avoid broken image requests */}
+        {tileUrl && (
+            <TileLayer
+                key={tileUrl}
+                url={tileUrl}
+                tms={false} // Both OSM and Earth Engine tiles use standard XYZ (tms=false)
+                attribution={ndvi ? '&copy; Google Earth Engine, LusoAlerta' : baseMapAttribution}
+            />
+        )}
+        <MapControl onToggle={() => setNdvi((s) => !s)} />
         </MapContainer>
     );
 }
