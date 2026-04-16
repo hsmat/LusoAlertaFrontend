9d48c51079501e23df174a732229775ce1e38e87
hsmat <hsmat@iscte-iul.pt>
2026-04-17 00:43:04 +0100

    epa não sei

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import 'leaflet/dist/leaflet.css';
-import MapViewer from './MapViewer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginForm from "./LoginForm.jsx";
+import Dashboard from "./Dashboard.jsx";
 
 function App() {
-  return (
-    <MapViewer>
-    </MapViewer>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
