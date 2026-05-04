191e0832474b7fb1a530e45fc5e6d11db3d003ca
fccba <154232906+fccba@users.noreply.github.com>
2026-05-04 18:07:15 +0100

    mudar /login para / correcao de 1 erro

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<LoginForm />} />
+                <Route path="/" element={<LoginForm />} />
                 <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </BrowserRouter>
--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Navigate } from "react-router-dom";
 import './Login.css';
     
 export default function Login() {
